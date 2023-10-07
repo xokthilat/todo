cdb0c54dcebf597b394753cfaeef662b1442ef08
mikasaloli <xokthilat@gmail.com>
2023-10-07 10:58:40 +0700

    Revert integration test command

--- run_integration_test.sh
+++ run_integration_test.sh
@@ -3,31 +3,31 @@
 # Initialize counters
 success_count=0
 fail_count=0
-
+running_number=100
 # Create a directory to store failure logs
 mkdir -p failure_logs
 
 # Define the log file
 log_file="test_results.log"
 
-# Run tests in an infinite loop until the terminal is terminated
-while true; do
-  echo "Running tests..."
+# Loop 100 times
+for ((i=1; i<=running_number; i++)); do
+  echo "Running test $i..."
   output=$(flutter test integration_test 2>&1)
   if [[ $output == *"All tests passed"* ]]; then
-    echo "Test: Success"
+    echo "Test $i: Success"
     ((success_count++))
   else
-    echo "Test: Fail"
+    echo "Test $i: Fail"
     ((fail_count++))
     # Save the failure reason to a file
-    echo "$output" > "failure_logs/failure_$fail_count.log"
+    echo "$output" > "failure_logs/failure_$i.log"
   fi
-
-  # Save results to the log file
-  echo "Total Success: $success_count" > "$log_file"
-  echo "Total Fail: $fail_count" >> "$log_file"
-
-  # Print the results to the console
-  cat "$log_file"
 done
+
+# Save results to the log file
+echo "Total Success: $success_count" >> "$log_file"
+echo "Total Fail: $fail_count" >> "$log_file"
+
+# Print the results to the console
+cat "$log_file"
